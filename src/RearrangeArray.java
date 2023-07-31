91d9ffac3bcd3324403ee56f253f021404da0050
vimleshxrpaswan <paswanvimlesh481@gmail.com>
2023-08-01 00:02:57 +0530

    question added

--- src/RearrangeArray.java
+++ src/RearrangeArray.java
@@ -1,5 +1,13 @@
 import java.util.Arrays;
 
+//Given an unsorted integer array containing many duplicate elements, rearrange it such that the same element appears together and the relative order of the
+//first occurrence of each element remains unchanged.
+//For example:
+//Input:  { 1, 2, 3, 1, 2, 1 }
+//Output: { 1, 1, 1, 2, 2, 3 }
+//Input:  { 5, 4, 5, 5, 3, 1, 2, 2, 4 }
+//Output: { 5, 5, 5, 4, 4, 3, 1, 2, 2 }
+
 public class RearrangeArray {
     public static void main(String[] args) {
         int[] input = {5, 4, 5, 5, 3, 1, 2, 2, 4};
